7093c54885b5ca50601b18473fe0d6a6d5a547d2
will3g <willianpaulo_09@hotmail.com>
2020-01-07 09:31:44 -0300

    fix-responsive-in-mobile-and-add-new-section

--- public/js/test.js
+++ public/js/test.js
@@ -15,3 +15,19 @@ const req = (() => {
                     title.appendChild(document.createTextNode(res.title))
              });
 })();
+
+let img2 = document.getElementById('image-three');
+let title2 = document.getElementById('title-three');
+
+const req3 = (() => {
+       fetch('https://api.nasa.gov/planetary/apod?api_key=J2bmLrlUbNdxgPBQqkvNMpl4uIP1mE52WYrpuOpq')
+                .then(res => res.json())
+                .then(res => { 
+                       img2.style.background = `url(${res.url})`,
+                       img2.style.backgroundSize = 'contain',
+                       img2.style.backgroundRepeat = 'no-repeat',
+                       img2.style.backgroundSize = 'cover';
+                       img2.style.filter = 'brightness(1.5) contrast(1.2)',
+                       title2.appendChild(document.createTextNode(res.title))
+                });
+   })();
