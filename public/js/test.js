195de1075e739a674a389126c4cbd4b45098a652
will3g <willianpaulo_09@hotmail.com>
2020-01-16 09:57:33 -0300

    test-requests

--- public/js/test.js
+++ public/js/test.js
@@ -1,33 +1,54 @@
 // https://api.nasa.gov/planetary/apod?api_key=J2bmLrlUbNdxgPBQqkvNMpl4uIP1mE52WYrpuOpq
 
-let img = document.getElementById('image-two');
-let title = document.getElementById('title-two');
+function render(nameTagImg, nameTagTitle, jsonImage, jsonTitle) {
+       let img = document.getElementById(nameTagImg);
+       let title = document.getElementById(nameTagTitle);
+
+       img.style.background = `url(${jsonImage})`;
+       img.style.backgroundRepeat = 'no-repeat';
+       img.style.backgroundSize = 'cover';
+       img.style.filter = 'brightness(1.5) contrast(1.2)';
+       img.style.backgroundPosition = 'center, center';
+       title.appendChild(document.createTextNode(jsonTitle));
+}
 
 const req = (() => {
+       let img = document.getElementById('image-two');
+       let title = document.getElementById('title-two');
     fetch('https://api.nasa.gov/planetary/apod?api_key=J2bmLrlUbNdxgPBQqkvNMpl4uIP1mE52WYrpuOpq')
              .then(res => res.json())
              .then(res => { 
-                    img.style.background = `url(${res.url})`,
-                    img.style.backgroundSize = 'contain',
-                    img.style.backgroundRepeat = 'no-repeat',
-                    img.style.backgroundSize = 'cover';
-                    img.style.filter = 'brightness(1.5) contrast(1.2)',
-                    title.appendChild(document.createTextNode(res.title))
+                    const image = res.url;
+                    const title = res.title;
+                    render('image-two', 'title-two', image, title);
              });
 })();
 
-let img2 = document.getElementById('image-three');
-let title2 = document.getElementById('title-three');
+const req2 = (() => {
+
+       fetch('https://images-api.nasa.gov/search?q=all%2019&description=planets%20landing&media_type=image')
+                .then(res => res.json())
+                .then(res => {
+                     const image = res.collection.items[0].links[0].href;
+                     const title = res.collection.items[0].data[0].title;
+                     render('image-three', 'title-three', image, title);
+                });
+   })();
 
 const req3 = (() => {
-       fetch('https://api.nasa.gov/planetary/apod?api_key=J2bmLrlUbNdxgPBQqkvNMpl4uIP1mE52WYrpuOpq')
+       fetch('https://images-api.nasa.gov/search?q=apollo%2011&description=moon%20landing&media_type=image')
                 .then(res => res.json())
                 .then(res => { 
-                       img2.style.background = `url(${res.url})`,
-                       img2.style.backgroundSize = 'contain',
-                       img2.style.backgroundRepeat = 'no-repeat',
-                       img2.style.backgroundSize = 'cover';
-                       img2.style.filter = 'brightness(1.5) contrast(1.2)',
-                       title2.appendChild(document.createTextNode(res.title))
+                       for (const item in res.collection.items) {
+                              if (res.collection.items.hasOwnProperty(item)) {
+                                     const news = res.collection.items[item];
+                                   //   console.log('imagem: '+news.links[0].href);
+                                   //   console.log('fotógrafo: '+news.data[0].photographer);
+                                   //   console.log('local: '+news.data[0].location);
+                                   //   console.log('keywords: '+news.data[0].keywords);
+                                   //   console.log('descrição: '+news.data[0].description);
+                                   //   console.log('-----------------------');
+                              }
+                       }
                 });
    })();
