0289f1eebf89f2ab19dd6618dd7ca9847d2cf016
will3g <willianpaulo_09@hotmail.com>
2020-01-30 09:15:15 -0300

    creating-server-side-rendering-in-nodejs

--- src/app.js
+++ src/app.js
@@ -4,9 +4,18 @@ const routesHome = require('./routesHome');
 class App {
     constructor() {
         this.server = express();
+        this.corsing();
         this.routes();
     }
     routes() { this.server.use(routesHome); }
+
+    corsing() {
+        this.server.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
+    }
 }
 
 module.exports = new App().server;
