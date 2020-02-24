25bfb92501707e01bebcafc68359a90c7b34cd9b
will3g <willianpaulo_09@hotmail.com>
2020-02-24 16:24:14 -0300

    deploy

--- public/server.js
+++ public/server.js
@@ -1,15 +1,24 @@
-let http = require('http');
+let https = require('https');
+let fs = require('fs');
 let express = require('express');
 
 let app = express();
 
-http.createServer(app).listen('8000', function() {
+https.createServer({
+
+    key: fs.readFileSync('./ssl/server.key'),  // CHAVE DE SEGUNRANÇA PARA HTTPS COM SENHA
+    cert: fs.readFileSync('./ssl/server.crt'), // CERTIFICADO DE SEGURANÇA COM SENHA DO SERVIDOR
+    ca: fs.readFileSync('./ssl/ca.crt'),       // CERTIFICADO DE SEGURANÇA
+    requestCert: true,
+    rejectUnauthorized: false
+
+}, app).listen('8443', function() {
 
     // RENDERIZA OS ARQUIVOS ESTÁTICOS EM TELA
     app.use(express.static('../public'))
 
     // TENTEI CHAMAR O SERVICE WORKER, PORÉM ELE NÃO É CARREGADO POR CONTA DOS CERTIFICADOS QUE ESTÃO INVÁLIDOS!
-    app.use(express.static('./register-sw.js'))
+    app.use(express.static('../public/js/register-sw.js'))
 
-    console.log("Rodando na porta 8000 em HTTP");
+    console.log("Rodando na porta 8443 em HTTPS com certificados de segurança");
 });
